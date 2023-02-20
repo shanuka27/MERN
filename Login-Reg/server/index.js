5b57d8c585077fedfbc9cb7f152bc5f05b86da17
shanuka27 <shanukayasiru27@gmail.com>
2023-02-20 22:45:28 +0530

    backend

--- Login-Reg/server/index.js
+++ Login-Reg/server/index.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user.model')
 
-app.get('/hello', (req, res)=>{
-    res.send('hello world')
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/full-mern')
+
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
+	try {
+		// const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+        console.log(err)
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
+})
+
+app.post('/api/login', async (req, res)=>{
+    const user =  await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+    })
+    if(user) {
+        return res.json({ status: 'ok', user: true})
+    }else{
+        return res.json({ status: 'error', user: false})
+    }
 })
 
 app.listen(8082, ()=> {

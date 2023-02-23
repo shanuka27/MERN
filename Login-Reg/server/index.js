6adb82039e846b180d476b603ae712552b8c6220
shanuka27 <shanukayasiru27@gmail.com>
2023-02-23 13:21:53 +0530

    final commit

--- Login-Reg/server/index.js
+++ Login-Reg/server/index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 app.use(cors())
 app.use(express.json())
@@ -12,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/full-mern')
 app.post('/api/register', async (req, res) => {
 	console.log(req.body)
 	try {
-		// const newPassword = await bcrypt.hash(req.body.password, 10)
+		const newPassword = await bcrypt.hash(req.body.password, 10)
 		await User.create({
 			name: req.body.name,
 			email: req.body.email,
@@ -20,23 +22,72 @@ app.post('/api/register', async (req, res) => {
 		})
 		res.json({ status: 'ok' })
 	} catch (err) {
-        console.log(err)
 		res.json({ status: 'error', error: 'Duplicate email' })
 	}
 })
 
-app.post('/api/login', async (req, res)=>{
-    const user =  await User.findOne({
-            email: req.body.email,
-            password: req.body.password,
-    })
-    if(user) {
-        return res.json({ status: 'ok', user: true})
-    }else{
-        return res.json({ status: 'error', user: false})
-    }
+app.post('/api/login', async (req, res) => {
+	const user = await User.findOne({
+		email: req.body.email,
+	})
+
+	if (!user) {
+		return { status: 'error', error: 'Invalid login' }
+	}
+
+	const isPasswordValid = await bcrypt.compare(
+		req.body.password,
+		user.password
+	)
+
+	if (isPasswordValid) {
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email: user.email,
+			},
+			'secret123'
+		)
+
+		return res.json({ status: 'ok', user: token })
+	} else {
+		return res.json({ status: 'error', user: false })
+	}
 })
 
-app.listen(8082, ()=> {
-    console.log('sever started on 8082')
+app.get('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		const user = await User.findOne({ email: email })
+
+		return res.json({ status: 'ok', quote: user.quote })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
+
+app.post('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		await User.updateOne(
+			{ email: email },
+			{ $set: { quote: req.body.quote } }
+		)
+
+		return res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
+
+app.listen(8082, () => {
+	console.log('Server started on 8082')
 })
